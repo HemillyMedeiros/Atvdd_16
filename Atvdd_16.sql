bb1e2b4c9bb18cf7bd98b6d06d4568dad9e390ce
Hemilly Nicole <hemilly.santos@fatec.sp.gov.br>
2023-11-19 17:59:42 -0300

    A(16): escrevendo um cursor que exibe todos os nomes dos youtubers em ordem reversa

--- Atvdd_16.sql
+++ Atvdd_16.sql
@@ -17,3 +17,31 @@ BEGIN --2 abrindo cursor
     CLOSE cur_youtubers; 
 END; 
 $$
+
+-- 2 Escreva um cursor que exibe todos os nomes dos youtubers em ordem reversa. Para tal O SELECT deverá ordenar em ordem não reversa;
+-- - O Cursor deverá ser movido para a última tupla;
+-- - Os dados deverão ser exibidos de baixo para cima;
+
+DO $$
+DECLARE 
+--1 Declarando cursor
+    cur_reverse REFCURSOR;
+    tupla RECORD;
+BEGIN -- 2 abrindo cursor
+    OPEN cur_reverse SCROLL FOR --SCROLL É PARA ROLAR DE BAIXO PARA CIMA
+        SELECT * FROM tb_top_youtubers;
+    LOOP
+    --3 Recuperando dados de interesse
+        FETCH cur_reverse INTO tupla;
+        EXIT WHEN NOT FOUND;
+        --oque esta a direita do ponto é membro de quem esta a esquerda do ponto
+    END LOOP;
+    LOOP
+        FETCH BACKWARD FROM cur_reverse INTO tupla;
+        EXIT WHEN NOT FOUND;
+        RAISE NOTICE '%', tupla.youtuber;
+    END LOOP;
+    --4 fechando o cursor
+    CLOSE cur_reverse;
+END;
+$$
